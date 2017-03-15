bb711b5aa7e0b3c0f3d8dd64cfd03f62ac7d1c63
nlicitra <nickerous@gmail.com>
2017-03-15 19:34:41 -0400

    Osc v1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 require("file-loader?name=[name].[ext]!./index.html")
 const D3 = require("d3")
 
-// const path = "../audio/dragon.mp3"
-
 D3.select("body").append("span").text("D3 Works")
 
 const ctx = new AudioContext()
@@ -12,16 +10,12 @@ const source = ctx.createMediaElementSource(audio)
 const analyser  = ctx.createAnalyser()
 
 source.connect(analyser)
-analyser.connect(ctx.destination)
+source.connect(ctx.destination)
+
 analyser.fftSize = 2048;
-const bufferLength = analyser.frequencyBinCount;
+const bufferLength = 200 || analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
-analyser.getByteTimeDomainData(dataArray);
-
-// const dest = ctx.createMediaStreamDestination()
-// source.connect(dest)
-// source.connect(ctx.destination)
-
+analyser.getByteFrequencyData(dataArray);
 
 // Get file
 function loadFile(evt) {
@@ -31,3 +25,34 @@ function loadFile(evt) {
 }
 document.getElementById('file').addEventListener('change', loadFile, false);
 
+const createSVG = (parent, height, width) => {
+    return D3.select(parent)
+             .append("svg")
+             .attr("height", height)
+             .attr("width", width)
+}
+
+const HEIGHT = 300
+const WIDTH = 1200
+const svg = createSVG("body", HEIGHT, WIDTH)
+
+svg.selectAll("rect")
+    .data(dataArray)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * (WIDTH / dataArray.length))
+    .attr("width", Math.max((WIDTH / dataArray.length) - 1, 0))
+
+function render () {
+    requestAnimationFrame(render)
+
+    // console.log(dataArray)
+    analyser.getByteFrequencyData(dataArray);
+    svg.selectAll('rect')
+        .data(dataArray)
+        .attr("y", (d) => HEIGHT - d)
+        .attr("height", (d) => d)
+        .attr("fill", (d) => `rgb(144,200,${d})`)
+}
+
+render()
