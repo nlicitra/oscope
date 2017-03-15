fd1383aab85d39d9bd91bd8b0fc264414cb98fc5
nlicitra <nickerous@gmail.com>
2017-03-15 18:43:57 -0400

    D3 works and audio nodes and stuff

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 require("file-loader?name=[name].[ext]!./index.html")
+const D3 = require("d3")
 
 // const path = "../audio/dragon.mp3"
-const audio = document.body.querySelector("#audio")
 
+D3.select("body").append("span").text("D3 Works")
 
 const ctx = new AudioContext()
+
+const audio = document.body.querySelector("#audio")
 const source = ctx.createMediaElementSource(audio)
-const dest = ctx.createMediaStreamDestination()
-source.connect(dest)
-source.connect(ctx.destination)
-const recorder = new MediaRecorder(dest.stream, {mimeType: "audio/webm"})
+const analyser  = ctx.createAnalyser()
+
+source.connect(analyser)
+analyser.connect(ctx.destination)
+analyser.fftSize = 2048;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+
+// const dest = ctx.createMediaStreamDestination()
+// source.connect(dest)
+// source.connect(ctx.destination)
+
 
 // Get file
 function loadFile(evt) {
