e19d6578408c9b0e7bf2c7fa16b694fc4166838b
nlicitra <nickerous@gmail.com>
2017-03-15 21:30:03 -0400

    Bumpin

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require("file-loader?name=[name].[ext]!./index.html")
+const lodash = require("lodash")
 const D3 = require("d3")
 
-D3.select("body").append("span").text("D3 Works")
 
 const ctx = new AudioContext()
 
@@ -43,6 +43,23 @@ svg.selectAll("rect")
     .attr("x", (d, i) => i * (WIDTH / dataArray.length))
     .attr("width", Math.max((WIDTH / dataArray.length) - 1, 0))
 
+const throttledRemove = lodash.debounce(() => {
+        console.log("removing")
+        D3.select("#bumper").classed("bump", false)
+}, 500)
+
+const throttledAdd = lodash.throttle(() => {
+        console.log("adding")
+        D3.select("#bumper").classed("bump", true)
+        throttledRemove()
+}, 500)
+
+function triggerBump() {
+    throttledAdd()
+}
+
+const THRESHOLD = 0.8 * 255
+const INDEX = 5
 function render () {
     requestAnimationFrame(render)
 
@@ -53,6 +70,17 @@ function render () {
         .attr("y", (d) => HEIGHT - d)
         .attr("height", (d) => d)
         .attr("fill", (d) => `rgb(144,200,${d})`)
+
+    if (dataArray[INDEX] > THRESHOLD) {
+        console.log("triggered", dataArray[INDEX])
+        triggerBump()
+    }
 }
+
+// setTimeout(() => D3.select("#bumper").classed("bump", true), 0)
+// setTimeout(() => {
+//     D3.select("#bumper").classed("bump", false)
+// }, 2000)
+
 
 render()
