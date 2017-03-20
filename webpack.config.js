0bed71da7fd3033f091ce2fbd9b6003ca5e161eb
nlicitra <nickerous@gmail.com>
2017-03-19 23:17:35 -0400

    Actually added all the files :D

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + "/src/index.html",
+    filename: "index.html",
+    inject: "body"
+})
+
 module.exports = {
     entry: "./src/app.js",
     output: {
         path: __dirname + "/dist",
-        publicPath: '/dist/',
+        publicPath: '/app',
         filename: "bundle.js"
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader']
+                // test: /\.css$/,
+                // loaders: ['style-loader', 'css-loader']
+            // }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [HTMLWebpackPluginConfig]
 }
