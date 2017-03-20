f9b0a17e8a8c88d8633ef8f9a5884911849f9960
nlicitra <nickerous@gmail.com>
2017-03-19 23:22:18 -0400

    Revert "Actually added all the files :D"
    This reverts commit 0bed71da7fd3033f091ce2fbd9b6003ca5e161eb.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,16 @@
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-    template: __dirname + "/src/index.html",
-    filename: "index.html",
-    inject: "body"
-})
-
 module.exports = {
     entry: "./src/app.js",
     output: {
         path: __dirname + "/dist",
-        publicPath: '/app',
+        publicPath: '/dist/',
         filename: "bundle.js"
     },
     module: {
         rules: [
             {
-                // test: /\.css$/,
-                // loaders: ['style-loader', 'css-loader']
-            // }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: 'babel-loader'
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader']
             }
         ]
-    },
-    plugins: [HTMLWebpackPluginConfig]
+    }
 }
